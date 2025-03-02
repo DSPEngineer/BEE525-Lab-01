70bf7ce54f9a49a63438386e9e5e7701ed6df181
Jose Pagan <dspengineer@optonline.net>
2025-03-01 21:13:56 -0800

    Completed input loop for count values

--- hw_01.py
+++ hw_01.py
@@ -13,28 +13,40 @@ print("        Jose Pagan & Vincent Dang")
 ## Initialize and Start the display thread
 th = displayThread.displayThread()
 th.start()
+count = " "
 
-## Get user's input
-count = int( input("Enter seconds to count: " ) )
-print(f"Count Down from: {count}")
+while count != "exit":
+    ## Get user's input
+    ## count = int( input("Enter seconds to count: " ) )
+    count = input("Enter seconds to count (\"exit\" to quit): " )
+    if ( count == "exit" ) :
+        continue
+    elif ( len( count) != 1 ) | ( count < '0' ) | ( count > '9' ) :
+        print(f"ERROR: Invalid input: {count} ")
+        print(f"       Please enter a value btween 1 amd 9, or \"exit\" to quit")
+        continue
+    else:
+        x=int( count )
 
-x=count
-start_time = datetime.now()
-while x > 0:
+
+    print(f"Count Down from: {count}")
+
+    start_time = datetime.now()
+    while x > 0:
+        th.setDisplay(x)
+        sleep(1)
+        x -= 1
+
+    # Capture time at end of countdoen loop
+    end_time = datetime.now()
+
+    ## Display the final zero digit
     th.setDisplay(x)
-    sleep(1)
-    x -= 1
 
-# Capture time at end of countdoen loop
-end_time = datetime.now()
-
-## Display the final zero digit
-th.setDisplay(x)
-
-print(f"--- Countdown comlete:")
-print(f"---         Start: { start_time.strftime('%H:%M:%S.%f')} " )
-print(f"---           End: { end_time.strftime('%H:%M:%S.%f')} " )
-print(f"--- Total Elapsed: { (end_time - start_time)} ")
+    print(f"--- Countdown comlete:")
+    print(f"---         Start: { start_time.strftime('%H:%M:%S.%f')} " )
+    print(f"---           End: { end_time.strftime('%H:%M:%S.%f')} " )
+    print(f"--- Total Elapsed: { (end_time - start_time)} ")
 
 
 ## Clear the display
